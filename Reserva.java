0584a9ca12d5622c7c501ab276c77c12f9f216c0
ruanpabllobc <112910621+ruanpabllobc@users.noreply.github.com>
2024-04-20 20:58:38 -0300

    Classes Reserva e Usuário
    A lógica foi reestruturada para a implementação com o banco de dados

--- Reserva.java
+++ Reserva.java
@@ -1,54 +1,164 @@
-package restaurante;
+package src.restaurante;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import src.db.Banco;
 
 public class Reserva {
-    private String dataReserva;
-    private String turno;
+
+    private int id;
+    private Date dataReserva;
     private int quantidadePessoas;
     private float valor;
     private String metodoPagamento;
+    private String idMesa;
+    private String idRestaurante;
+    private String idCliente;
 
-    private static List<Reserva> reservas = new ArrayList<>();
-
-    public Reserva(String dataReserva, String turno, int quantidadePessoas, float valor, String metodoPagamento) {
+    public Reserva(Date dataReserva, int quantidadePessoas, float valor, String metodoPagamento, 
+    String idRestaurante, String idCliente, String idMesa) {
         this.dataReserva = dataReserva;
-        this.turno = turno;
         this.quantidadePessoas = quantidadePessoas;
         this.valor = valor;
         this.metodoPagamento = metodoPagamento;
-        reservas.add(this);
-        System.out.println("Sucesso.");
-    }
-
-    public void visualizarReserva() {
-        System.out.println("Data: " + dataReserva);
-        System.out.println("Turno: " + turno);
-        System.out.println("Quantidade de Pessoas: " + quantidadePessoas);
-        System.out.println("Valor: " + valor);
-        System.out.println("Método de Pagamento: " + metodoPagamento);
-    }
-
-    public static void listarReservas() {
-        System.out.println("Lista de reservas:");
-        for (Reserva reserva : reservas) {
-            reserva.visualizarReserva();
-            System.out.println("-----------");
-        }
+        this.idRestaurante = idRestaurante;
+        this.idCliente = idCliente;
+        this.idMesa = idMesa;
     }
     
-    public void atualizarReserva(String novaData, String novoTurno, int novaQuantidadePessoas, float novoValor, String novoMetodoPagamento) {
-        this.dataReserva = novaData;
-        this.turno = novoTurno;
-        this.quantidadePessoas = novaQuantidadePessoas;
-        this.valor = novoValor;
-        this.metodoPagamento = novoMetodoPagamento;
-        System.out.println("Sucesso.");
+    public Reserva(){
     }
 
-    public void cancelarReserva() {
-        reservas.remove(this);
-        System.out.println("Sucesso.");
+    public void cadastrarReserva(Banco db) {
+        System.out.println("controle");
+        String query = String.format("INSERT INTO Reserva (data_reserva, quantidade_pessoas, valor, metodo_pagamento, restaurante, cliente, numero_mesa) VALUES ('%tF', '%d', '%.2f', '%s', %s, %s, '%d')", 
+        getDataReserva(), getQuantidadePessoas(), getValor(), getMetodoPagamento(), getIdRestaurante(), getIdCliente(), getIdMesa());
+        db.queryUpdate(query);
+    }
+
+    public void pesquisarReserva(Banco db, int reservaId) throws SQLException {
+        String query = String.format("SELECT * FROM Reserva WHERE id_reserva = %d", reservaId);
+        ResultSet rs = db.querySearch(query);
+    
+        setId(rs.getInt("id_reserva"));
+        String datast = rs.getString("data_reserva");
+        Date data = Date.valueOf(datast);
+        setDataReserva(data);
+        setQuantidadePessoas(rs.getInt("quantidade_pessoas"));
+        setValor(rs.getFloat("valor"));
+        setMetodoPagamento(rs.getString("metodo_pagamento"));
+        setIdRestaurante(rs.getString("restaurante"));
+        setIdCliente(rs.getString("cliente"));
+        setIdMesa(rs.getString("numero_mesa"));
+    }
+
+    public static boolean pesquisarReservaNoDia(Banco db, Date data, String mesaId, String restauranteId) throws SQLException {
+        String query = String.format("SELECT * FROM Reserva WHERE data_reserva = '%tF' and numero_mesa = '%s' and restaurante = '%s'", data, mesaId, restauranteId);
+        ResultSet rs = db.querySearch(query);
+       
+        if (rs.next()){
+            System.out.println("Mesa indisponivel para a data");
+            return true;
+        }
+        return false;
+    }
+    
+    
+    public void listarReservas(Banco db) throws SQLException {
+        String query = "SELECT * FROM Reserva";
+        ResultSet rs = db.querySearch(query);
+
+        while (rs.next()){
+            setId(rs.getInt("id_reserva"));
+            String datast = rs.getString("data_reserva");
+            Date data = Date.valueOf(datast);
+            setDataReserva(data);
+            setQuantidadePessoas(rs.getInt("quantidade_pessoas"));
+            setValor(rs.getFloat("valor"));
+            setMetodoPagamento(rs.getString("metodo_pagamento"));
+            setIdRestaurante(rs.getString("restaurante"));
+            setIdMesa(rs.getString("numero_mesa"));
+            System.out.println(toString());
+            }
+    }
+
+    public void editarReserva(Banco db) {
+        String query = String.format("UPDATE Reserva SET numero_mesa = '%s', data_reserva = '%s', quantidade_pessoas = '%d', valor = '%.2f', metodo_pagamento = '%s', restaurante = '%s', cliente = '%s' WHERE id = %d",getIdMesa(), getDataReserva(), getQuantidadePessoas(), getValor(), getMetodoPagamento(), getIdRestaurante(), getIdCliente(), getId());
+        db.queryUpdate(query);
+    }
+
+    public void removerReserva(Banco db) {
+        String query = String.format("DELETE FROM Reserva WHERE id = %d", getId());
+        db.queryUpdate(query);
+    }
+    
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public Date getDataReserva() {
+        return dataReserva;
+    }
+
+    public void setDataReserva(Date dataReserva) {
+        this.dataReserva = dataReserva;
+    }
+   
+    public int getQuantidadePessoas() {
+        return quantidadePessoas;
+    }
+
+    public void setQuantidadePessoas(int quantidadePessoas) {
+        this.quantidadePessoas = quantidadePessoas;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public String getMetodoPagamento() {
+        return metodoPagamento;
+    }
+
+    public void setMetodoPagamento(String metodoPagamento) {
+        this.metodoPagamento = metodoPagamento;
+    }
+
+    public String getIdMesa(){
+        return idMesa;
+    }
+
+    public void setIdMesa(String idMesa){
+        this.idMesa = idMesa;
+    }
+
+    public String getIdRestaurante() {
+        return idRestaurante;
+    }
+
+    public void setIdRestaurante(String idRestaurante) {
+        this.idRestaurante = idRestaurante;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva {id=" + getId() + ", dataReserva=" + getDataReserva() + ", quantidadePessoas=" + getQuantidadePessoas() + ", valor=" + getValor() + ", metodoPagamento=" + getMetodoPagamento() + ", mesa=" + getIdMesa() + ", restaurante=" + getIdRestaurante() + ", cliente=" + getIdCliente() + "}";
     }
 }
--- Usuario.java
+++ Usuario.java
@@ -1,97 +1,47 @@
-package restaurante;
+package src.restaurante;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Usuario {
+public abstract class Usuario {
     private String nome;
     private String senha;
-    private int usuarioID;
+    private String email;
 
-    private static List<Usuario> usuarios = new ArrayList<>();
-
-    public Usuario(String nome, String senha, int usuarioID) {
+    public Usuario(String nome, String senha, String email){
         this.nome = nome;
         this.senha = senha;
-        this.usuarioID = usuarioID;
-        usuarios.add(this);
-        System.out.println("Sucesso.");
+        this.email = email;
     }
-    public void fazerLogin(int usuarioID, String senha) {
-        if (this.usuarioID == usuarioID && this.senha.equals(senha)) {
-             System.out.println("Login realizado");
-         } else {
-             System.out.println("Usuário ou senha incorretos.");
-         }
-     }
-    public void alterarSenha(String senha, String novaSenha) {
-        if (this.senha.equals(senha)) {
-                this.senha = novaSenha;
-                System.out.println("Senha alterada");
-            } else {
-            System.out.println("Senha atual incorreta");
-        }
+
+    public Usuario(){
     }
-    public static void visualizarUsuario(int usuarioID) {
-        for (Usuario usuario : usuarios) {
-            if (usuario.getUsuarioID() == usuarioID) {
-                System.out.println("Nome: " + usuario.getNome());
-                System.out.println("ID: " + usuario.getUsuarioID());
-                System.out.println("Senha: " + usuario.getSenha());
-                return;
-            }
-        }
-        System.out.println("Usuário não encontrado.");
-    }
-    public static void atualizarUsuario(int usuarioID, String novoNome, String novaSenha) {
-        for (Usuario usuario : usuarios) {
-            if (usuario.getUsuarioID() == usuarioID) {
-                usuario.setNome(novoNome);
-                usuario.setSenha(novaSenha);
-                return;
-            }
-        }
-        System.out.println("Usuário não encontrado.");
-    }
-    public static void excluirUsuario(int usuarioID) {
-        for (Usuario usuario : usuarios) {
-            if (usuario.getUsuarioID() == usuarioID) {
-                usuarios.remove(usuario);
-                System.out.println("Usuário excluído");
-                return;
-            }
-        }
-        System.out.println("Usuário não encontrado.");
-    }
+
+    abstract void usuarioLogin(String login, String senha);
+
     public String getNome() {
         return nome;
     }
-    public String getSenha() {
-        return senha;
-    }
-    public int getUsuarioID() {
-        return usuarioID;
-    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public String getSenha() {
+        return senha;
+    }
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
-    public void setUsuarioID(int usuarioID) {
-        this.usuarioID = usuarioID;
+    
+    public String getEmail() {
+        return email;
     }
-    public static void listarUsuarios() {
-        if (usuarios.isEmpty()) {
-            System.out.println("Lista vazia.");
-        } else {
-            System.out.println("Lista de usuários:");
-            for (Usuario usuario : usuarios) {
-                System.out.println("Nome: " + usuario.getNome());
-                System.out.println("ID: " + usuario.getUsuarioID());
-                System.out.println("Senha: " + usuario.getSenha());
-                System.out.println("-----------");
-            }
-        }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario {nome=" + getNome() + ", senha=" + getSenha() + ", email=" + getEmail() + "}";
     }
 }
